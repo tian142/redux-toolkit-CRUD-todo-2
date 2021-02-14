5cdddf0da4650964e7ece5074a5b6e7a54c0ecf1
Tian <tian.qiu142@gmail.com>
2021-02-14 12:31:51 -0800

    completed addTodo Logic by dispatching reducer in InsertTodo.js

--- src/features/todo/InsertTodo.js
+++ src/features/todo/InsertTodo.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { useDispatch } from "react-redux"
+
+import { addTodo } from "./todoSlice"
 
 export const InsertTodo = () => {
+  const [todo, setTodo] = useState("")
+  const onTodoChange = (e) => setTodo(e.target.value)
+
+  const dispatch = useDispatch()
+
+  const onAddTodo = () => dispatch(addTodo(todo))
+
   return (
     <div>
       <span>
-        <input />
+        <input value={todo} onChange={onTodoChange} />
       </span>
-      <button>Add</button>
+      <button onClick={onAddTodo}>Add</button>
     </div>
   )
 }
