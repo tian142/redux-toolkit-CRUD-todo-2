d44d604c6e5cf54661891f632c2861b5b451e081
Tian <tian.qiu142@gmail.com>
2021-02-14 12:17:52 -0800

    added react router to project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import { InsertTodo } from "./features/todo/InsertTodo"
 import { TodoList } from "./features/todo/TodoList"
 import { Navbar } from "./Navbar"
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <InsertTodo />
-      <TodoList />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <InsertTodo />
+                <TodoList />
+              </>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   )
 }
