e60b8bc1c1a90adc6e5e62ef7479ee683c5b2225
Tian <tian.qiu142@gmail.com>
2021-02-14 12:25:51 -0800

    populated todoSlice with addTodo reducer and prepared payload with id and todo

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,25 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: [],
+  reducers: {
+    addTodo: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(todo) {
+        return {
+          payload: {
+            id: nanoid(),
+            todo,
+          },
+        }
+      },
+    },
+  },
+})
+
+export const { addTodo } = todoSlice.actions
+
+export default todoSlice.reducer
