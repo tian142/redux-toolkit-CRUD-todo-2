13501ca8c2e1ff9db3a643cf50afcf57687de08e
Tian <tian.qiu142@gmail.com>
2021-02-14 12:19:10 -0800

    added home Link in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export const Navbar = () => {
   return (
     <div>
       <h1>A Really Good Todo List</h1>
+      <Link to="/">Home</Link>
+      <hr />
     </div>
   )
 }
